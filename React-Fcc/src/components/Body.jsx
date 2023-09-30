dce15e17948562109fc4ee3123748d26d72a0991
Zouziszzm <farhumaid@gmail.com>
2023-09-30 11:33:03 +0530

    Day 6 update-2

--- React-Fcc/src/components/Body.jsx
+++ React-Fcc/src/components/Body.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
   const [list, setList] = useState([]);
@@ -25,11 +26,24 @@ const Body = () => {
       const filteredList = list.filter((restaurant) =>
         restaurant.info.name.toLowerCase().includes(val.toLowerCase()),
       );
+
       setList(filteredList);
+      setNoItemFound = false;
+      // Check if the filtered list is empty
+      setTimeout(() => {
+        if (filteredList.length === 0) {
+          setNoItemFound(true); // Set a state variable to indicate "No item found"
+        } else {
+          setNoItemFound(false); // Reset the state variable if items are found
+        }
+      }, "2000");
     }
   }
   //will add a simmer effect after the conditional rendering.
-  return (
+  //
+  return list.length <= 1 ? (
+    <Shimmer />
+  ) : (
     <>
       <div className="center">
         <input
@@ -42,16 +56,31 @@ const Body = () => {
         <button onClick={handleSearch}>Search</button>
       </div>
       <div className="blockholdedr">
-        {list.map((pre) => {
-          return (
+        {list.length > 0 ? (
+          list.map((pre) => (
             <div className="blocks" key={pre.info.id}>
               <h2>{pre.info.name}</h2>
               <h3>{pre.info.cuisines.join(", ")}</h3>
               <h4>{pre.info.cloudinaryImageId}</h4>
               <p>{pre.info.avgRating}</p>
             </div>
-          );
-        })}
+          ))
+        ) : // Display "No item found" message when no items are found
+        noItemFound ? (
+          <>
+            <div className="center">
+              <input
+                type="text"
+                value={val}
+                onChange={(e) => {
+                  setVal(e.target.value);
+                }}
+              />
+              <button onClick={handleSearch}>Search</button>
+            </div>
+            <h1>No item found</h1>
+          </>
+        ) : null}
       </div>
     </>
   );
---
+++ React-Fcc/src/components/Shimmer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Shimmer = () => {
+  return <div className="center">shimmer ui loading</div>;
+};
+
+export default Shimmer;

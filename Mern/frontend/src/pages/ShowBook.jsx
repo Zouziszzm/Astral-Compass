85da1b7bdad6479f3d9bfb64598575033b9161bd
Zouziszzm <farhumaid@gmail.com>
2023-10-01 23:05:29 +0530

    pt push -3.

--- Mern/frontend/src/pages/ShowBook.jsx
+++ Mern/frontend/src/pages/ShowBook.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 const ShowBook = () => {
-  return <div>Show Book</div>;
+  return <div>Show Books</div>;
 };
 
 export default ShowBook;

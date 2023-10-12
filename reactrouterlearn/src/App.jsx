6a790bb272846d5046a9c86202dc51ed5236189c
Al Farhaan khan I Inamdar <63239385+Zouziszzm@users.noreply.github.com>
2023-10-12 05:32:12 -0700

    git react router

--- reactrouterlearn/src/App.jsx
+++ reactrouterlearn/src/App.jsx
@@ -1,28 +1,36 @@
-import Home from './components/Home'
-import Layout from './components/Layout'
-import About from './components/About'
-import Contact from './components/Contact'
-import User from './components/User'
-import Github from './components/Github'
-import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom'
-function App() {
+import React from 'react';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 
+import Home from './components/Home';
+import Layout from './components/Layout';
+import About from './components/About';
+import Contact from './components/Contact';
+import User from './components/User';
+import Github from './components/Github';
+
+function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<Layout />}>
         <Route path="/" element={<Home />} />
-        <Route path="About" element={<About />} />
-        <Route path="Contact" element={<Contact />} />
-        <Route path="User/:id" element={<User />} />
-        <Route path="Github" element={<Github />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="user/:id" element={<User />} />
+        <Route loader={gitinfo} path="github" element={<Github />} />
       </Route>
     )
-  )
+  );
+
   return (
     <>
       <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+async function gitinfo() {
+  const res = await fetch('https://api.github.com/users/Zouziszzm');
+  return res.json();
+}
+
+export default App;
--- reactrouterlearn/src/components/Github.jsx
+++ reactrouterlearn/src/components/Github.jsx
@@ -1,19 +1,13 @@
-import { data } from 'autoprefixer'
-import React from 'react'
-import { useState } from 'react'
-import { useEffect } from 'react'
-
+import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 const Github = () => {
-
-  const [usekar, setUsekar] = useState([])
-
-  useEffect(() => {
-    fetch("https://api.github.com/users/Zouziszzm").then(res => res.json()).then(data => setUsekar(data))
-  }, [])
+  const data = useLoaderData(); // Assuming gitinfo returns a Promise
 
   return (
-    <div className='text-center text-3xl bg-red-400 text-white'>Github followers: {usekar.followers}</div>
-  )
-}
+    <div className='text-center text-3xl bg-red-400 text-white'>
+      Github followers: {data.followers}
+    </div>
+  );
+};
 
-export default Github
+export default Github;
